36d69a66f99673649860f56a4aaf5f34a3c64077
TejasBhurbhure <tejasbhurbhure06@gmail.com>
2025-03-06 01:54:48 +0530

    remove unused animations.css import from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import './styles/animations.css';
 import './App.css';
 
 function App() {
